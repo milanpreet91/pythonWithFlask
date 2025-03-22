719d4de77eb85af7e161d848b453f2b96f98e130
milanpreet91 <kaur.milanpreet7@gmail.com>
2025-03-22 09:33:25 +0000

    Basic flask app

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+app = Flask(__name__) # name references app.py file
+#app is object of class Flask
+#__name__ : refers to how the script is invoked. 
+# If invoked from app.py it refers to __main__
+#print(__name__): prints __main__
+
+#Tell flask when a certain URL is requested, 
+# what shuld be returned.
+#Thats why we register a route
+#<domain_name>/<route>: jovian.com/profile
+#Use decorator for registering
+#when url / accessed, print "hello_world"
+@app.route('/') #pass url route for home
+def index():
+    return "Hello World"
+
+#if running app.py as a script, 
+# then start the app using below command
+if __name__ == "__main__":
+    app.run(debug=True)
